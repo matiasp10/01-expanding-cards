92a0546842b49c07e8d8cc135d926c99e4b220bd
Matias Petenatti <matiasodin96@gmail.com>
2023-10-13 20:29:49 -0300

    add comments

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// con el querySelectorAll selecciono todos los contenedores con la clase ".panel"
+// con un forEach a cada panel le agrego un evento que se escuche cuando le haga click
+// este evento llama a una funcion que remueve las clases "active" a todos los paneles
+// luego de ejecutar esa funcion le agrego la clase "active" al panel clickeado
+
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
